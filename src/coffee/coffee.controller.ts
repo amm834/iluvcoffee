86cfb3a591a6804c18901487d35dc1f594d797cd
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-07-18 14:03:52 +0630

    feat: add query

--- src/coffee/coffee.controller.ts
+++ src/coffee/coffee.controller.ts
@@ -1,17 +1,22 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 
 @Controller('coffee')
 export class CoffeeController {
-  findAll() {
-    return 'This action returns all coffees';
+  @Get()
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+    return `This action returns all coffee. Limit: ${limit}, offset: ${offset}`;
   }
 
   @Get(':id')
@@ -23,5 +28,15 @@ export class CoffeeController {
   @HttpCode(HttpStatus.GONE)
   create(@Body() body) {
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
   }
 }
